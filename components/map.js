186f3f093404497ee447b3c1992805b86d2ba845
MOHAN RAJ <mohanraj.2k00@gmail.com>
2022-05-03 10:10:21 +0530

    initial commit

--- components/map.js
+++ components/map.js
@@ -1,23 +1,39 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  SVGOverlay,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const Map = () => {
+  const bounds = [
+    [59.49, -0.08],
+    [51.5, -0.06],
+  ];
   return (
     <MapContainer
       center={[51.505, -0.09]}
-      zoom={13}
       scrollWheelZoom={false}
-      style={{ height: 400, width: "100%" }}
+      maxZoom={3}
+      zoom={0}
+      style={{ height: "80vh", width: "100%", marginTop: "5rem" }}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        minZoom={0}
+        maxZoom={3}
+        noWrap={false}
+        url="../map/{z}_{x}_{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <SVGOverlay attributes={{ stroke: "red" }} bounds={bounds}>
+        <rect x="0" y="0" width="100%" height="100%" fill="blue" />
+        <circle r="5" cx="10" cy="10" fill="red" />
+        <text x="50%" y="50%" stroke="white">
+          text
+        </text>
+      </SVGOverlay>
     </MapContainer>
   );
 };
--- pages/LeafletMap.js
+++ pages/LeafletMap.js
@@ -4,13 +4,10 @@ import dynamic from "next/dynamic";
 function LeafletMap() {
   const Map = useMemo(
     () =>
-      dynamic(
-        () => import("../components/map"), // replace '@components/map' with your component's location
-        {
-          loading: () => <p>A map is loading</p>,
-          ssr: false, // This line is important. It's what prevents server-side render
-        }
-      ),
+      dynamic(() => import("../components/map"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false, // This line is important. It's what prevents server-side render
+      }),
     [
       /* list variables which should trigger a re-render here */
     ]
